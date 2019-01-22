a0234f29d1f898a9947e4ad539b207b039ea6f62
Deniz Dalkilic <denizdalkilic1998@gmail.com>
2019-01-22 14:15:26 +0300

    First Commit
    Added Project to Github

---
+++ Augmented Reality (Dolar-TL)/Assets/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,149 @@
+
+using UnityEngine;
+using Vuforia;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+using UnityEngine.Networking;
+using System;
+using System.Xml;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+/// 
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{   
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    protected int FindFirstLetterInString(string str) {
+       
+            for (int ctr = 0; ctr < str.Length; ctr++)
+            {
+                if (Char.IsLetter(str[ctr]))
+                {
+                    return ctr;
+                }
+            }
+            return -1;
+        
+    }
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            string name = mTrackableBehaviour.TrackableName;
+            Debug.Log("Trackable " + name + " found");
+
+            //initialize info
+            if (TargetLister.currency != name.Substring(FindFirstLetterInString(name), name.Length - FindFirstLetterInString(name))) {
+                TargetLister.currency = name.Substring(FindFirstLetterInString(name), name.Length - FindFirstLetterInString(name));
+                TargetLister.total = 0;
+            }
+
+            TargetLister.total += Convert.ToDouble(name.Substring(0, FindFirstLetterInString(name)));
+            Debug.Log("<color=green>Currency name: " + TargetLister.currency + "</color>");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+    #endregion // PROTECTED_METHODS
+}
---
+++ Augmented Reality (Dolar-TL)/Assets/Scripts/TargetLister.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+using UnityEngine.Networking;
+using System;
+using System.Xml;
+
+
+/**
+    * The Controller Class which accesses and retrieves currency rate information from floatrates.com
+    * and displays it on the canvas according to the active Image Target's properties 
+ */
+
+public class TargetLister : MonoBehaviour
+{
+   
+    //Static Variables                          
+    public static double total = 0;        // Total amount of money 
+    public static string currency = "try"; //initialized with Turkish Lira
+
+    //Public Variables (Initialized by the editor)
+    public Text text_equivalent, exchangeRate, text_total;
+    public GameObject up, down, set, reset, panelRate, panelTotal, panelResult;
+
+    //Private Variables
+    private enum InfoType { Rate, Description };
+    private string xml_file;
+    private int rateIndex;
+    private string currencyTemp;            //used for checking whether the currency is changed or not
+    private InfoType infoType;
+    private List<CountryNode> countryNodes; //a list that holds countries and their information
+
+    //inner class for country information
+    public class CountryNode 
+    {
+        public string countryName;
+        public string description;
+        public double exchangeRate;
+        public double inverseRate;
+
+        public CountryNode(string name, double rate, string invDescription, double invRate)
+        {
+            countryName = name;
+            description = invDescription;
+            exchangeRate = rate;
+            inverseRate = invRate;
+        }
+    }
+    
+    // Methods
+
+    void Start()
+    {
+        rateIndex = 0;
+        currencyTemp = currency;
+        infoType = InfoType.Rate;
+        countryNodes = new List<CountryNode>();
+        StartCoroutine(GetText());
+    }
+
+    void Update()
+    {
+        if (currency != currencyTemp) //if the new currency is different from the old one
+        {
+            currencyTemp = currency;
+            rateIndex = 0;
+            StartCoroutine(GetText());
+        }
+
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+        IList<TrackableBehaviour> tbs = (IList<TrackableBehaviour>)sm.GetActiveTrackableBehaviours();
+
+        up.SetActive(tbs.Count != 0);
+        down.SetActive(tbs.Count != 0);
+        set.SetActive(tbs.Count != 0);
+        reset.SetActive(tbs.Count != 0);
+        panelRate.SetActive(tbs.Count != 0);
+        panelResult.SetActive(tbs.Count != 0);
+        panelTotal.SetActive(tbs.Count != 0);
+        text_equivalent.text = "";
+        exchangeRate.text = "";
+        text_total.text = "";
+       
+        foreach (TrackableBehaviour tb in tbs)
+        {
+            //Value of Total is updated by TargetImages (DefaultTrackableEventHandler Script)
+            text_equivalent.text = (string)countryNodes[rateIndex].countryName + ": " + Math.Round(total / (double)countryNodes[rateIndex].inverseRate, 3);
+
+            text_total.text = "Total: " + total;
+
+            if (infoType == InfoType.Rate)
+            {
+                exchangeRate.fontSize = 14;
+                exchangeRate.text = "Rate: " + countryNodes[rateIndex].inverseRate;
+                //Debug.Log(countryNodes[rateIndex].inverseRate);
+            }
+            else if (infoType == InfoType.Description)
+            {
+                exchangeRate.fontSize = 10;
+                exchangeRate.text = "" + countryNodes[rateIndex].description;
+            }
+        }
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://www.floatrates.com/daily/"+currency+".xml");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            //Assign the xml to a string
+            xml_file = www.downloadHandler.text;
+            
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+           
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+
+            countryInfoExtractor(xml_file);
+          
+        }
+        StopCoroutine(GetText());
+    }
+
+   
+    private void countryInfoExtractor(string xmlFile) { //xml parser for retrieving country info
+        countryNodes = new List<CountryNode>(); //old values disappear or kept in stack ?
+        XmlDocument xml = new XmlDocument();
+        xml.LoadXml(xmlFile); 
+
+        XmlNodeList xnList = xml.SelectNodes("/channel/item");
+   
+        foreach (XmlNode xn in xnList)
+        {
+            string targetName = xn["targetName"].InnerText;
+            string description = xn["inverseDescription"].InnerText;
+            double exchangeRate = Math.Round(Convert.ToDouble(xn["exchangeRate"].InnerText),3);
+            double inverseRate = Math.Round(Convert.ToDouble(xn["inverseRate"].InnerText),3);
+            countryNodes.Add(new CountryNode(targetName, exchangeRate, description, inverseRate));
+        }
+    }
+
+    public void increaseRateIndex() {
+        if (rateIndex == countryNodes.Count-1)
+            rateIndex = 0;
+        else
+            rateIndex++;
+    }
+
+    public void decreaseRateIndex()
+    {
+        if (rateIndex == 0)
+            rateIndex = countryNodes.Count-1;
+        else
+            rateIndex--;
+    }
+
+    public void resetTotal() {
+        total = 0;
+    }
+
+    public void infoTypeToggle() {
+        if (infoType == InfoType.Rate)
+            infoType = InfoType.Description;
+        else
+            infoType = InfoType.Rate;
+    }
+}
